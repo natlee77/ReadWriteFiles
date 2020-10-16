7d437126445570011e64ea383de733554e20519a
natlee77 <natlisjo@gmail.com>
2020-10-16 14:48:12 +0200

    Add project files.

---
+++ ReadWriteJsonFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using SharedClassLibrary;
+using System;
+
+namespace ReadWriteJsonFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //JsonService.WriteToFile(@"d:\files\persons.json", new Person("Hans", "Mattin-Lassei", 36, "Farsta"));
+            //JsonService.ReadFromFile(@"d:\files\persons.json");
+            JsonService.WriteToFileCorrect(@"d:\files\persons.json", new Person("Hans", "Mattin-Lassei", 36, "Farsta"));
+        }
+    }
+}
---
+++ ReadWriteTextFile/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ReadWriteTextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string fileName = "persons.txt"; //läg direct till f.
+
+            WriteToFile("persons.txt", "Nataliya.Lisjö,49,Degerfors");//
+            ReadFromFile("persons.txt");
+
+            //WriteToFile("textfile.txt", "My brothers name is Tommy. ");
+        }
+
+        private static void WriteToFile(string filename, string text)// spara info i fil
+        {//en Lista
+            var lines = new List<string>();      //namn lines
+            lines.Add(text);                     // mer dynamisk
+
+           // File.WriteAllLines(@$"C:\Nataliya\.NET\DataLagring\Cataloq_files{filename}", lines); 
+           //skriver alla rader som finns i lines.Add(text)och skriver over-ersäter alt och rewrite  info nästa gång ;/
+
+             File.AppendAllLines(@$"C:\Nataliya\.NET\DataLagring\Cataloq_files{filename}", lines);
+            //skapa fil +add -med samma info varja gång
+           
+            
+            
+            
+            {             
+               //File.AppendAllText(@$"d:\files\{filename}", content); //class from System.IO
+            }
+        }
+
+        private static void ReadFromFile(string filename)// läsa from file
+        {
+            Console.WriteLine("Using array "); //array
+            var lines2 = File.ReadAllLines(@$"C:\Nataliya\.NET\DataLagring\Cataloq_files{filename}");
+                foreach  (var line in lines2)
+                {
+                    Console.WriteLine(line);
+                }
+
+
+            Console.WriteLine("Using List ");
+            var lines = File.ReadAllLines(@$"C:\Nataliya\.NET\DataLagring\Cataloq_files{filename}").ToList(); //++Using system.linq
+               foreach(var line in lines)
+               {
+                     Console.WriteLine(line);
+               }
+
+            
+           // var content = File.ReadAllText(@$"d:\files\{filename}");
+           // Console.WriteLine(content);
+        }
+
+    }
+}
